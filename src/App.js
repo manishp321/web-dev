1d8a057718db78ed2eb245bb831f208246ebf85f
manishp321 <manishpanthi040@gmail.com>
2021-10-02 05:34:26 +0300

    header done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './Title.css'
+import Title from './Title';
+
+function App() {
+  return (
+    <div className="App">
+      <Title/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Title.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Title() {
+  return (
+    <div className='Title'>
+      <h1>The New York Times</h1>
+      <hr></hr>
+    </div>
+  )
+}
+
+export default Title
